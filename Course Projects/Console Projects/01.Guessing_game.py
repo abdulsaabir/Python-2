688d3faa74a01589858a32c572ecc88f12c7d9bc
abdulsaabir <saabirhaaji10@gmail.com>
2023-11-05 14:07:08 +0300

    lesson 9

--- Course Projects/Console Projects/01.Guessing_game.py
+++ Course Projects/Console Projects/01.Guessing_game.py
@@ -3,7 +3,7 @@ import random
 # Generate a random number within the specified range
 random_number = random.randint(0, 100)
 
-# Completing the Project
+# Complete the Project
 # -----------------------
 
 # Your task is to extend and complete the number guessing game. Follow these steps to finish the project:
@@ -12,7 +12,7 @@ random_number = random.randint(0, 100)
 # 2. ASK the user to enter their guess.
 # 3. CHECK if the user's input is a valid number.
 # 4. COMPARE the user's guess to the randomly generated number we generated in line 4.
-# 5. KEEP TRACK of the number of guesses the user makes.
+# 5. KEEP TRACK of the number of guesses the user makes if the user input was a number.
 # 6. PRINT the GUESS result based on whether the user guessed correctly in one attempt or took multiple attempts.
 
 
